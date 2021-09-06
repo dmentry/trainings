942ddba1f8c1d3c45624d3e262e3602491637b6a
dack99 <slipin2020@mail.ru>
2021-09-06 22:51:29 +0300

    Improve logic of statistic's graphs

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,51 +1,40 @@
 class StatisticsController < ApplicationController
   def stat
-    @charts = User.chart_statuses
+    @chart_types = User.chart_statuses
     
     if params[:chart_name_id].to_i <= 0
       @chart_id = User.chart_statuses[User.first.chart_status]
-
     else
       @chart_id = params[:chart_name_id].to_i
 
       User.first.update(chart_status: User.chart_statuses.key(@chart_id))
     end
 
+    list_temp = []
 
-
-@list_temp = []
-@list = []
-    # @list = ExerciseNameVoc.all.pluck(:id, :label)
+    @exercises_list = []
 
     Exercise.all.each do |exercise|
-
-      @list_temp << exercise.exercise_name_voc.label
-
+      list_temp << exercise.exercise_name_voc.label
     end
 
-    @list_temp.uniq!
+    list_temp.uniq!
 
-        ExerciseNameVoc.all.each do |exercise|
-          @list << [exercise.id, exercise.label] if @list_temp.include?(exercise.label)
-
-# binding.pry
+    ExerciseNameVoc.all.each do |exercise|
+      @exercises_list << [exercise.id, exercise.label] if list_temp.include?(exercise.label)
     end
-
-# @list
-
 
     @data = []
 
     if params[:exercise_name_id].to_i <= 0
       if Exercise.count <= 1 && Training.count <= 1
-        redirect_to trainings_url, alert: "У вас еще нет данных для статистики."
+        redirect_to trainings_url, alert: "У вас еще недостаточно данных для статистики."
       else
-      id = Exercise.first.exercise_name_voc_id
-    end
+        id = Exercise.first.exercise_name_voc_id
+      end
     else
       id = params[:exercise_name_id].to_i
     end
-
 
     @name = Exercise&.find_by(exercise_name_voc: id)&.exercise_name_voc&.label
 
