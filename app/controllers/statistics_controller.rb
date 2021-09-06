e30c2162018a0ba1eff22cf30a95cb460d0edc1e
dack99 <slipin2020@mail.ru>
2021-09-06 23:53:09 +0300

    Add all exercises mode to statistic's graphs

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -24,6 +24,8 @@ class StatisticsController < ApplicationController
       @exercises_list << [exercise.id, exercise.label] if list_temp.include?(exercise.label)
     end
 
+    @exercises_list << [(@exercises_list.count+1), 'Все упражнения']
+
     @data = []
 
     if params[:exercise_name_id].to_i <= 0
@@ -36,16 +38,33 @@ class StatisticsController < ApplicationController
       id = params[:exercise_name_id].to_i
     end
 
-    @name = Exercise&.find_by(exercise_name_voc: id)&.exercise_name_voc&.label
+    unless id == @exercises_list.count
+      @name = Exercise&.find_by(exercise_name_voc: id)&.exercise_name_voc&.label
 
-    Training.all.each do |training|
-      training.exercises.each do |exercise|
-        if exercise.exercise_name_voc_id == id
-          @data << [training.start_time, exercise.summ]
+      Training.all.each do |training|
+        training.exercises.each do |exercise|
+          if exercise.exercise_name_voc_id == id
+            @data << [training.start_time, exercise.summ]
+          end
         end
+      end
+    else
+      (@exercises_list.count - 1).times do |i|
+        data_temp = []
+        Training.all.each do |training|
+          training.exercises.each do |exercise|
+            if exercise.exercise_name_voc_id == i + 1
+              data_temp << [training.start_time, exercise.summ]
+            end
+          end
+        end
+
+        data_temp = data_temp.sort
+
+        @data << {name: @exercises_list[i][1], data: data_temp}
       end
     end
 
-    @data = @data.sort
+    @data
   end
 end
