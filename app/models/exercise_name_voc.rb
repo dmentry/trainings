f6f144f9944230c49405b01cfc6e157bd3105830
dack99 <slipin2020@mail.ru>
2021-10-15 22:43:38 +0300

    Fix issue

--- app/models/exercise_name_voc.rb
+++ app/models/exercise_name_voc.rb
@@ -3,5 +3,5 @@ class ExerciseNameVoc < ApplicationRecord
   belongs_to :user
 
   validates :label, presence: true, length: { maximum: 45, message: "Не более 45 символов!" }
-  validates :label, uniqueness: { case_sensitive: false, message: "Такое название упражнения уже есть!" }
+  validates :label, uniqueness: { scope: :user, case_sensitive: false, message: "Такое название упражнения уже есть!" }
 end
